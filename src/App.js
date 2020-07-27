5717a5e4e9e6e272033a912b98b9d2fb97e63e64
th3uuz <matheuslcd@hotmail.com>
2020-07-27 18:55:44 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 import Menu from './components/Menu'
-// import BannerMain from './components/BannerMain'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import dadosIniciais from './data/dados_iniciais.json'
+import Footer from './components/Footer'
+
+import './app.css'
 
 function App() {
   return (
-    <div>
+    <div style={{ background: '#141414' }}>
       <Menu />
-      {/* <BannerMain /> */}
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].tiulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Escute só essa música."}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Footer />
     </div>
   );
 }

64e3e2fc7a009bae38bd74da41e0cd0d649d44c2
th3uuz <matheuslcd@hotmail.com>
2020-07-28 11:28:38 -0300

    include categorys in page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Video
+    </div>
+  )
+}
+
+const Pagina404 = () => (<div>Página 404</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route exact path="/" component={App} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
